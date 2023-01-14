53a971f93e8a3a161db03869ea40bc6a82e11c92
vishweswar <91923370+vishweswar@users.noreply.github.com>
2023-01-14 17:15:28 -0500

    Create waves.py

---
+++ waves.py
@@ -0,0 +1,12 @@
+class waves: 
+    
+    dVar = []
+    
+    def __init__(self, wave, frequency, length):
+        if(wave == "Square" or wave == "square"):
+           frequency = abs(frequency)
+           if(frequency == 0):
+            
+    def fn(self, a):
+        a.setValue(300)
+        
