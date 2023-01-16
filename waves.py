d1bc81a67c2dc304d0eb54d01f79a289c554c5c8
vishwa <vish.eswaran@gmail.com>
2023-01-16 17:19:20 -0500

    testVer1

--- waves.py
+++ waves.py
@@ -6,7 +6,16 @@ class waves:
         if(wave == "Square" or wave == "square"):
            frequency = abs(frequency)
            if(frequency == 0):
+            print("HELLO")
             
+
     def fn(self, a):
         a.setValue(300)
+        print("HELLO")
+
+        
+
+
+
+
         
